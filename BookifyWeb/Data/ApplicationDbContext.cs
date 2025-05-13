72fe8bf98111dabee7feeb147849c41e8f8dce95
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-05-13 12:48:11 +0600

    Add Entity Framework Core and Category management
    Updated project to include EF Core packages. Added ApplicationDbContext for category management with migrations to create and manage the 'categories' table. Configured SQL Server connection in appsettings.json and registered the context in Program.cs.

---
+++ BookifyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookifyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookifyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Category> categories { get; set; }
+    }
+}
