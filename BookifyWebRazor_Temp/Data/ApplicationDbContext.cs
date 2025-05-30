5c89de03c3c7338344da373dd103af9ab79e9d9a
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-05-30 09:09:59 +0600

    small change in Category Model add BookifyWebRazor_Temp new project

---
+++ BookifyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BookifyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BookifyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuidler)
+        {
+            modelBuidler.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Scifi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+     
+    }
+}
